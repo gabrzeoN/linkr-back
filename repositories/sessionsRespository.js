9f56472b9f5571c49213148cf3b7397a40dbe926
Gabriel Cari <g.cari@hotmail.com>
2022-06-15 13:19:02 -0300

    feat: logout done.

--- repositories/sessionsRespository.js
+++ repositories/sessionsRespository.js
@@ -7,3 +7,23 @@ export async function insertNewSession(userId, token){
         [userId, token]
     );
 }
+
+export async function selectSession(token){
+    const session = await db.query(`
+        SELECT *
+        FROM sessions
+        WHERE token = $1 AND status = true;`,
+        [token]
+    );
+    return session.rows[0];
+}
+
+export async function invalidateSession(token){
+    await db.query(`
+        UPDATE sessions
+        SET status = false
+        WHERE token = $1 AND status = true;`,
+        [token]
+    );
+    return;
+}
--- routers/authRouter.js
+++ routers/authRouter.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { signIn, signUp } from "./../controllers/authController.js";
+import { signIn, signUp, signOut } from "./../controllers/authController.js";
 import { signInSchema, userSchema } from "../schemas/authSchemas.js";
 import validSchema from "../middwares/validateSchemaMiddware.js";
+import validToken from "../middwares/validateTokenMiddware.js";
 
 const authRouter = Router();
 
 authRouter.post("/sign-in", validSchema(signInSchema), signIn);
 authRouter.post('/sign-up', validSchema(userSchema), signUp);
+authRouter.patch('/sign-out', validToken, signOut);
 
 export default authRouter;
