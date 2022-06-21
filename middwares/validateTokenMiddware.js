1e7f32d40a1bdb4325c287c7f5e29eca07319557
Geicy Beatriz Soudré Cardoso <geicybeatriz@gmail.com>
2022-06-21 15:59:22 -0300

    fix: test edit post

--- middwares/validateTokenMiddware.js
+++ middwares/validateTokenMiddware.js
@@ -4,10 +4,13 @@ export default async function validToken(req, res, next){
     const {authorization} = req.headers;
     const token = authorization?.replace("Bearer", "").trim();
     if(!token) return res.status(401).send("Token not found!");
+    console.log("passei token", token);
+
     try{
         const session = await selectSession(token);
         if(!session) return res.status(401).send("You are logged off, please sign in first!");
         res.locals.session = session;
+        console.log("passei sessão", session);
         next();
     }catch (err){
         return res.sendStatus(500);
