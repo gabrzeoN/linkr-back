754117aa328a4171b8c62f7d548c7f7ae7277e88
Geicy Beatriz Soudré Cardoso <geicybeatriz@gmail.com>
2022-06-23 07:15:04 -0300

    feat: get user posts

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,17 @@
 import urlMetadata from "url-metadata";
-import { getUserPosts } from "../repositories/usersRepository.js";
-
+import { getUserById, getUserPosts } from "../repositories/usersRepository.js";
 
 export async function getPostsByUserId(req, res){
     const {id} = req.params;
-    console.log(id);
+    console.log("id", id);
+    
     try {
+        const userData = await getUserById(parseInt(id));
+        if(userData.rowCount === 0) return res.status(404).send("user not found");
+
         const userPosts = await getUserPosts(parseInt(id));
-        console.log(userPosts.rows);
-        if(userPosts.rowCount === 0) return res.sendStatus(200);
+        console.log(userPosts);
+        if(userPosts.rowCount === 0) return res.status(200).send({userData: userData.rows[0], posts:[]});
 
         const posts = [...userPosts.rows];
         for(let i = 0; i < userPosts.rows.length; i++){
@@ -22,7 +25,8 @@ export async function getPostsByUserId(req, res){
                 }
             };
         }
-        return res.status(200).send(posts);
+        console.log(posts);
+        return res.status(200).send({userData: userData.rows[0], posts:posts});
     } catch (error) {
         console.log(error)
         res.sendStatus(500);
